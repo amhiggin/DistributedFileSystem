188e2e871cab27db943feb39559ce18be2267a4c
Amber <amhiggin@ITSERV.SCSS.TCD.IE>
2017-11-27 20:06:40 +0000

    adding a placeholder main method in the client application

--- Client.py
+++ Client.py
@@ -1,3 +1,17 @@
+#
+# This is the client application.
+# It will use a client API to communicate with the server (the client library).
+# Should be able to access the requested files by doing through the required servers.
+#
+
 import flask
 import flask_restful
 import sys, os
+
+
+def main():
+    return 0
+
+main()
+
+
