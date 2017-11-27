1bf166a2fe1a259625491100b273b3169f4e28a9
Amber <amhiggin@ITSERV.SCSS.TCD.IE>
2017-11-27 21:02:14 +0000

    ensuring that method calls to client api work properly

--- API.py
+++ API.py
@@ -11,20 +11,20 @@ import flask_restful
 # download a copy of the file
 # should inform lock server of the fact that it has a copy
 def read_file(file_path):
-    return "Hello World"
+    print "Request to read " + file_path
 
 
 # upload a changed copy of the file
 # should inform the lock server of the fact that it is updating a copy
 def write_file(file_path):
-    return "Hello World"
+    print "Request to write " + file_path
 
 
 # check the file exists on the fileserver, as such
 def open_file(file_path):
-    return "Hello World"
+    print "Request to open " + file_path
 
 
 # doesn't really do anything effective (from what I can see)
 def close_file(file_path):
-    return "Hello World"
+    print "Request to close " + file_path
