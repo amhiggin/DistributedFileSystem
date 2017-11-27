957ad5d962458e6d5b4911166bd5a596fd068e07
Amber <amhiggin@ITSERV.SCSS.TCD.IE>
2017-11-27 20:08:48 +0000

    adding the basic outline of the client api

---
+++ API.py
@@ -0,0 +1,19 @@
+#
+# This is the library of function calls that the client will be able to use.
+# It should allow for read, write, etc. (possibly locking also).
+#
+
+import os, sys
+
+
+def read_file():
+    # placeholder
+
+def write_file():
+    # placeholder
+
+def open_file():
+    # placeholder
+
+def close_file():
+    # placeholder
